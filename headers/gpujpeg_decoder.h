61f65bd3cee1eb67dd68af6bdd4ab0cad715a76e
Dr.Guo <xz.guo@hzgy-tech.com.cn>
2025-06-11 14:05:01 +0800

    Add initial setup for GPU JPEG Rust bindings
    This commit adds the initial boilerplate and bindings to interface with the GPUJPEG library from Rust. Key changes include:
    
    1. Added `Cargo.lock` with dependency tree
    2. Updated `Cargo.toml` with basic dependencies including:
       - `anyhow` for error handling
       - `log` for logging
       - `image` and `imageproc` for dev/testing
       - `bindgen` and `cmake` for build-time bindings generation
    
    3. Added `build.rs` build script to:
       - Link against libgpujpeg
       - Generate Rust bindings from C headers using bindgen
    
    4. Added header files from GPUJPEG:
       - Main header with common types/definitions
       - Encoder/decoder

---
+++ headers/gpujpeg_decoder.h
@@ -0,0 +1,286 @@
+/**
+ * @file
+ * Copyright (c) 2011-2025, CESNET
+ * Copyright (c) 2011, Silicon Genome, LLC.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef GPUJPEG_DECODER_H
+#define GPUJPEG_DECODER_H
+
+#ifndef __cplusplus
+#include <stdbool.h>
+#endif // __cplusplus
+
+#include "gpujpeg_common.h"
+#include "gpujpeg_type.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct gpujpeg_decoder;
+
+/**
+ * Decoder output type
+ */
+enum gpujpeg_decoder_output_type {
+    /// Decoder will use it's internal output buffer
+    GPUJPEG_DECODER_OUTPUT_INTERNAL_BUFFER,
+    /// Decoder will use custom output buffer
+    GPUJPEG_DECODER_OUTPUT_CUSTOM_BUFFER,
+    /// Decoder will use OpenGL Texture PBO Resource as output buffer
+    GPUJPEG_DECODER_OUTPUT_OPENGL_TEXTURE,
+    /// Decoder will use internal CUDA buffer as output buffer
+    GPUJPEG_DECODER_OUTPUT_CUDA_BUFFER,
+    /// Decoder will use custom CUDA buffer as output buffer
+    GPUJPEG_DECODER_OUTPUT_CUSTOM_CUDA_BUFFER,
+};
+
+/**
+ * Decoder output structure
+ */
+struct gpujpeg_decoder_output
+{
+    /// Output type
+    enum gpujpeg_decoder_output_type type;
+
+    /// Decompressed data
+    uint8_t* data;
+
+    /// Decompressed data size
+    size_t data_size;
+
+    /// Decoded image parameters
+    struct gpujpeg_image_parameters param_image;
+
+    /// OpenGL texture
+    struct gpujpeg_opengl_texture* texture;
+};
+
+/**
+ * @sa gpujpeg_parametes
+ * call gpujpeg_decoder_default_init_parameters() to initialize
+ */
+struct gpujpeg_decoder_init_parameters
+{
+    cudaStream_t stream; ///< stream CUDA stream to be used, cudaStreamDefault (0x00) is default
+    int verbose; ///< verbosity level (-1 - quiet, 0 - normal, 1 - verbose)
+    bool perf_stats; ///< print performance statistics on output
+    bool ff_cs_itu601_is_709; ///< if FFmpeg specific COM marker "CS=ITU601" present, interpret the data as
+                              ///< limited-range BT.709 not BT.601
+};
+
+/**
+ * Set default parameters to decoder output structure
+ *
+ * @param output  Decoder output structure
+ * @return void
+ */
+GPUJPEG_API void
+gpujpeg_decoder_output_set_default(struct gpujpeg_decoder_output* output);
+
+/**
+ * Setup decoder output to custom buffer
+ *
+ * @param output        Decoder output structure
+ * @param custom_buffer Custom buffer
+ * @return void
+ */
+GPUJPEG_API void
+gpujpeg_decoder_output_set_custom(struct gpujpeg_decoder_output* output, uint8_t* custom_buffer);
+
+/**
+ * Set decoder output to OpenGL texture
+ *
+ * @param output  Decoder output structure
+ * @return void
+ */
+GPUJPEG_API void
+gpujpeg_decoder_output_set_texture(struct gpujpeg_decoder_output* output, struct gpujpeg_opengl_texture* texture);
+
+/**
+ * Sets output to CUDA buffer
+ *
+ * @param output  Decoder output structure
+ */
+GPUJPEG_API void
+gpujpeg_decoder_output_set_cuda_buffer(struct gpujpeg_decoder_output* output);
+
+/**
+ * Setup decoder output to custom CUDA buffer
+ *
+ * @param output          Decoder output structure
+ * @param d_custom_buffer Custom buffer in CUDA device memory
+ * @return void
+ */
+GPUJPEG_API void
+gpujpeg_decoder_output_set_custom_cuda(struct gpujpeg_decoder_output* output, uint8_t* d_custom_buffer);
+
+/**
+ * Create JPEG decoder
+ *
+ * @sa gpujpeg_decoder_create_with_params
+ * @param stream CUDA stream to be used, may be cudaStreamDefault (0x00)
+ * @return decoder structure if succeeds, otherwise NULL
+ */
+GPUJPEG_API struct gpujpeg_decoder*
+gpujpeg_decoder_create(cudaStream_t stream);
+
+GPUJPEG_API struct gpujpeg_decoder_init_parameters
+gpujpeg_decoder_default_init_parameters(void);
+/**
+ * @brief Create JPEG decoder - extended versison
+ *
+ * This version is an alternative to gpujpeg_decoder_create() allowing setting more parameters during initialization
+ * (verbose, perf_stats). Previously, if those needed to be set, it the decoder must have been configured with
+ * gpujpeg_decoder_init().
+ *
+ * @sa gpujpeg_decoder_create
+ * @return decoder structure if succeeds, otherwise NULL
+ */
+GPUJPEG_API struct gpujpeg_decoder*
+gpujpeg_decoder_create_with_params(const struct gpujpeg_decoder_init_parameters *params);
+
+/**
+ * Init JPEG decoder for specific image properties
+ *
+ * Following properties are relevant:
+ * - image dimensions, commonent count
+ * - output pixel format that will be requested
+ * - interleaving, restart interval, color_space_internal (usually GPUJPEG_YCBCR_BT601_256LVLS)
+ * - correct subsampling setting
+ *
+ * @note
+ * Doesn't need to be called from user code, buffers will be initialized automatically according to
+ * image properties during decompression.
+ *
+ * @param decoder  Decoder structure
+ * @param[in] param        Parameters for coder, pointed structure is copied
+ * @param[in] param_image  Parameters for image data, pointed structure is copied
+ * @return 0 if succeeds, otherwise nonzero
+ */
+GPUJPEG_API int
+gpujpeg_decoder_init(struct gpujpeg_decoder* decoder, const struct gpujpeg_parameters* param, const struct gpujpeg_image_parameters* param_image);
+
+/**
+ * Decompress image by decoder
+ *
+ * @param decoder  Decoder structure
+ * @param image  Source image data
+ * @param image_size  Source image data size
+ * @param image_decompressed  Pointer to variable where decompressed image data buffer will be placed
+ * @param image_decompressed_size  Pointer to variable where decompressed image size will be placed
+ * @return @ref Errors
+ */
+GPUJPEG_API int
+gpujpeg_decoder_decode(struct gpujpeg_decoder* decoder, uint8_t* image, size_t image_size, struct gpujpeg_decoder_output* output);
+
+/**
+ * Returns duration statistics for last decoded image
+ * @return 0 if succeeds, otherwise nonzero
+ * @note
+ * The values are only informative and for debugging only and thus this is
+ * not considered as a part of a public API.
+ * @deprecated
+ * The decoder now prints the statistics to stdout if gpujpeg_parameters.perf_stats is set.
+ * May be removed in future versions - please report if using this function.
+ */
+GPUJPEG_DEPRECATED GPUJPEG_API int
+gpujpeg_decoder_get_stats(struct gpujpeg_decoder *decoder, struct gpujpeg_duration_stats *stats);
+
+/**
+ * Destory JPEG decoder
+ *
+ * @param decoder  Decoder structure
+ * @return 0 if succeeds, otherwise nonzero
+ */
+GPUJPEG_API int
+gpujpeg_decoder_destroy(struct gpujpeg_decoder* decoder);
+
+/**
+ * @defgroup decoder_pixfmt_placeholders
+ * @{
+ * following format placeholders are special values that may be passed
+ * to the decoeer in order to detect the format with optional constraints.
+ * Defined outside the enums to avoid -Wswitch warns.
+ */
+/// decoder default pixfmt - usually @ref GPUJPEG_444_U8_P012;
+/// @ref GPUJPEG_U8 for grayscale and @ref GPUJPEG_444_U8_P0123 if alpha present
+///
+#define GPUJPEG_PIXFMT_AUTODETECT ((enum gpujpeg_pixel_format)(GPUJPEG_PIXFMT_NONE - 1))
+/// as @ref GPUJPEG_PIXFMT_AUTODETECT, but alpha stripped if present
+#define GPUJPEG_PIXFMT_NO_ALPHA ((enum gpujpeg_pixel_format)(GPUJPEG_PIXFMT_AUTODETECT - 1))
+/// pixel format that may be stored in a PAM or Y4M file - a planar pixel
+/// format that is either 444, 422 or 420 for YUV, P012(3) otherwise
+#define GPUJPEG_PIXFMT_STD ((enum gpujpeg_pixel_format)(GPUJPEG_PIXFMT_NO_ALPHA - 1))
+/// @}
+/// Decode RGB for 3 or 4 channels, GPUJPEG_YCBCR for grayscale.
+/// decoder only, valid only if passed to gpujpeg_decoder_set_output_format()
+#define GPUJPEG_CS_DEFAULT ((enum gpujpeg_color_space)(GPUJPEG_NONE - 1))
+
+/**
+ * Sets output format
+ *
+ * If not called, @ref GPUJPEG_CS_DEFAULT and @ref GPUJPEG_PIXFMT_AUTODETECT
+ * are used.
+ *
+ * @param decoder         Decoder structure
+ * @param color_space     Requested output color space,
+ *                        use @ref GPUJPEG_NONE to keep JPEG internal color space;
+ *                        special value @ref GPUJPEG_CS_DEFAULT to decode RGB
+ *                        (or luma for grayscale)
+ * @param sampling_factor Requestd color sampling factor; special values
+ *                        @ref decoder_pixfmt_placeholders can be used
+ */
+GPUJPEG_API void
+gpujpeg_decoder_set_output_format(struct gpujpeg_decoder* decoder,
+                enum gpujpeg_color_space color_space,
+                enum gpujpeg_pixel_format pixel_format);
+
+/**
+ * @copydoc gpujpeg_reader_get_image_info
+ */
+GPUJPEG_API int
+gpujpeg_decoder_get_image_info(uint8_t *image, size_t image_size, struct gpujpeg_image_parameters *param_image, struct gpujpeg_parameters *param, int *segment_count);
+
+/// use RLE when writing TGA with gpujpeg_image_save_to_file (default is true), 0 or 1 please note that the option is
+/// global so it affects all decoder instances
+#define GPUJPEG_DEC_OPT_TGA_RLE_BOOL "dec_opt_tga_rle" ///< GPUJPEG_VAL_TRUE or GPUJPEG_VAL_FALSE
+/**
+ * sets decoder option
+ * @retval GPUJPEG_NOERR  option was sucessfully set
+ * @retval GPUJPEG_ERROR  invalid argument passed
+ */
+GPUJPEG_API int
+gpujpeg_decoder_set_option(struct gpujpeg_decoder* decoder, const char *opt, const char* val);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // GPUJPEG_DECODER_H
---
+++ headers/gpujpeg_encoder.h
@@ -0,0 +1,244 @@
+/**
+ * @file
+ * Copyright (c) 2011-2025, CESNET z.s.p.o
+ * Copyright (c) 2011, Silicon Genome, LLC.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef GPUJPEG_ENCODER_H
+#define GPUJPEG_ENCODER_H
+
+#include "gpujpeg_common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct gpujpeg_encoder;
+
+/**
+ * Encoder input type
+ */
+enum gpujpeg_encoder_input_type {
+    /// Encoder will use custom input buffer
+    GPUJPEG_ENCODER_INPUT_IMAGE,
+    /// Encoder will use OpenGL Texture PBO Resource as input buffer
+    GPUJPEG_ENCODER_INPUT_OPENGL_TEXTURE,
+    /// Encoder will use custom GPU input buffer
+    GPUJPEG_ENCODER_INPUT_GPU_IMAGE,
+};
+
+/**
+ * Encoder input structure
+ */
+struct gpujpeg_encoder_input
+{
+    /// Output type
+    enum gpujpeg_encoder_input_type type;
+
+    /// Image data
+    uint8_t* image;
+
+    /// Registered OpenGL Texture
+    struct gpujpeg_opengl_texture* texture;
+};
+
+/**
+ * Set encoder input to image data
+ *
+ * @param encoder_input  Encoder input structure
+ * @param image  Input image data
+ * @return void
+ * @sa gpujpeg_encoder_input_image
+ */
+GPUJPEG_API void
+gpujpeg_encoder_input_set_image(struct gpujpeg_encoder_input* input, uint8_t* image);
+
+/**
+ * Set encoder input to GPU image data
+ *
+ * @param encoder_input  Encoder input structure
+ * @param image GPU image data
+ * @return void
+ * @sa gpujpeg_encoder_input_gpu_image
+ */
+GPUJPEG_API void
+gpujpeg_encoder_input_set_gpu_image(struct gpujpeg_encoder_input* input, uint8_t* image);
+
+/**
+ * Set encoder input to OpenGL texture
+ *
+ * @param encoder_input  Encoder input structure
+ * @param texture_id  OpenGL texture id
+ * @return void
+ * @sa gpujpeg_encoder_input_set_texture
+ */
+GPUJPEG_API void
+gpujpeg_encoder_input_set_texture(struct gpujpeg_encoder_input* input, struct gpujpeg_opengl_texture* texture);
+
+/// alternative to @ref gpujpeg_encoder_input_set_image returning the struct as a return value
+GPUJPEG_API struct gpujpeg_encoder_input
+gpujpeg_encoder_input_image(uint8_t* image);
+/// alternative to @ref gpujpeg_encoder_input_set_gpu_image returning the struct as a return value
+GPUJPEG_API struct gpujpeg_encoder_input
+gpujpeg_encoder_input_gpu_image(uint8_t* image);
+/// alternative to @ref gpujpeg_encoder_input_set_texture returning the struct as a return value
+GPUJPEG_API struct gpujpeg_encoder_input
+gpujpeg_encoder_input_texture(struct gpujpeg_opengl_texture* texture);
+
+/**
+ * Create JPEG encoder
+ *
+ * @param stream CUDA stream to be used, may be cudaStreamDefault (0x00)
+ * @return encoder structure if succeeds, otherwise NULL
+ */
+GPUJPEG_API struct gpujpeg_encoder*
+gpujpeg_encoder_create(cudaStream_t stream);
+
+/**
+ * Compute maximum number of image pixels (width x height) which can be encoded by given memory size.
+ *
+ * @param encoder
+ * @param param
+ * @param param_image
+ * @param image_input_type
+ * @param memory_size
+ * @param max_pixels
+ * @return size of used device memory in bytes if succeeds, otherwise 0
+ */
+GPUJPEG_API size_t
+gpujpeg_encoder_max_pixels(struct gpujpeg_parameters * param, struct gpujpeg_image_parameters * param_image, enum gpujpeg_encoder_input_type image_input_type, size_t memory_size, int * max_pixels);
+
+/**
+ * Compute maximum size of device memory which will be used for encoding image with given number of pixels.
+ *
+ * @param encoder
+ * @param param
+ * @param param_image
+ * @param image_input_type
+ * @param max_pixels
+ * @return size of required device memory in bytes if succeeds, otherwise 0
+ */
+GPUJPEG_API size_t
+gpujpeg_encoder_max_memory(struct gpujpeg_parameters * param, struct gpujpeg_image_parameters * param_image, enum gpujpeg_encoder_input_type image_input_type, int max_pixels);
+
+/**
+ * Pre-allocate all encoding buffers for given image pixels.
+ *
+ * @param encoder
+ * @param param
+ * @param param_image
+ * @param image_input_type
+ * @return 0 if succeeds, otherwise nonzero
+ */
+GPUJPEG_API int
+gpujpeg_encoder_allocate(struct gpujpeg_encoder* encoder, const struct gpujpeg_parameters * param, const struct gpujpeg_image_parameters * param_image, enum gpujpeg_encoder_input_type image_input_type);
+
+/**
+ * Compress image by encoder
+ *
+ * @param encoder  Encoder structure
+ * @param param  Parameters for coder
+ * @param param_image  Parameters for image data
+ * @param image  Source image data
+ * @param[out] image_compressed  Pointer to variable where compressed image data buffer will be placed.
+ *                               Returned host buffer is owned by encoder and must not be freed by the caller.
+ *                               Buffer is valid until next gpujpeg_encoder_encode() call.
+ * @param[out] image_compressed_size  Pointer to variable where compressed image size will be placed.
+ * @return 0 if succeeds, otherwise nonzero
+ */
+GPUJPEG_API int
+gpujpeg_encoder_encode(struct gpujpeg_encoder* encoder, const struct gpujpeg_parameters* param,
+                       const struct gpujpeg_image_parameters* param_image, const struct gpujpeg_encoder_input* input,
+                       uint8_t** image_compressed, size_t* image_compressed_size);
+
+/**
+ * Returns duration statistics for last encoded image
+ * @return 0 if succeeds, otherwise nonzero
+ * @note
+ * The values are only informative and for debugging only and thus this is
+ * not considered as a part of a public API.
+ * @deprecated
+ * The encoder now prints the statistics to stdout if gpujpeg_parameters.perf_stats is set.
+ * May be removed in future versions - please report if using this function.
+ */
+GPUJPEG_DEPRECATED GPUJPEG_API int
+gpujpeg_encoder_get_stats(struct gpujpeg_encoder *encoder, struct gpujpeg_duration_stats *stats);
+
+enum gpujpeg_header_type {
+        GPUJPEG_HEADER_DEFAULT = 0, ///< for 1 or 3 channel @ref GPUJPEG_YCBCR_JPEG @ref GPUJPEG_HEADER_JFIF, for @ref
+                                    ///< GPUJPEG_RGB @ref GPUJPEG_HEADER_ADOBE, @ref GPUJPEG_HEADER_SPIFF otherwise
+        GPUJPEG_HEADER_JFIF    = 1<<0,
+        GPUJPEG_HEADER_SPIFF   = 1<<1,
+        GPUJPEG_HEADER_ADOBE   = 1<<2, ///< Adobe APP8 header
+};
+
+/**
+ * Forces JPEG header to be emitted.
+ *
+ * Header type should be capable of describing the resulting JPEG, eg. JFIF only for BT.601
+ * full-scale YCbCr images. If not, resulting JPEG image may be incompatible with decoders.
+ */
+GPUJPEG_API void
+gpujpeg_encoder_set_jpeg_header(struct gpujpeg_encoder *encoder, enum gpujpeg_header_type header_type);
+
+/**
+ * Suggests optimal restart interval to be used for given param_image balancing both image
+ * size and performance.
+ * @param subsampling 444 422 or 420
+ */
+GPUJPEG_API int
+gpujpeg_encoder_suggest_restart_interval(const struct gpujpeg_image_parameters* param_image,
+                                         gpujpeg_sampling_factor_t subsampling, bool interleaved, int verbose);
+
+#define GPUJPEG_ENCODER_OPT_OUT_PINNED  "enc_out_pinned" ///< deprecated - use GPUJPEG_ENC_OPT_OUT
+/// location of buffer returned from gpujpeg_encoder_encode()
+#define GPUJPEG_ENC_OPT_OUT          "enc_opt_out"
+#define GPUJPEG_ENC_OUT_VAL_PAGEABLE "enc_out_val_pageable"  ///< default
+#define GPUJPEG_ENC_OUT_VAL_PINNED   "enc_out_val_pinned"
+
+/**
+ * sets encoder option
+ * @retval GPUJPEG_NOERR  option was sucessfully set
+ * @retval GPUJPEG_ERROR  invalid argument passed
+ */
+GPUJPEG_API int
+gpujpeg_encoder_set_option(struct gpujpeg_encoder* encoder, const char* opt, const char* val);
+
+/**
+ * Destory JPEG encoder
+ *
+ * @param encoder  Encoder structure
+ * @return 0 if succeeds, otherwise nonzero
+ */
+GPUJPEG_API int
+gpujpeg_encoder_destroy(struct gpujpeg_encoder* encoder);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // GPUJPEG_ENCODER_H
