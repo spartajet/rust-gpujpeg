8d06937abac5398d98ea4c496b2b7aeb61ae3fa1
Dr.Guo <xz.guo@hzgy-tech.com.cn>
2025-06-11 14:32:15 +0800

    Update build configuration and dependencies
    - Add anyhow to build-dependencies
    - Remove gpujpeg linking from default build
    - Enable compiler lints for bindings
    - Comment out cmake build configuration for future reference

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
+#![allow(clippy::all)]
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
 include!("bindings.rs");
