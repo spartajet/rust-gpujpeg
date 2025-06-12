b67424df69f7556ec4223c5deeef50ff0c7fdca1
Dr.Guo <xz.guo@hzgy-tech.com.cn>
2025-06-12 22:29:01 +0800

    Build GPUJPEG from source instead of using prebuilt libs

--- build.rs
+++ build.rs
@@ -11,13 +11,17 @@ fn main() {
             .write_to_file("src/bindings.rs")
             .expect("Couldn't write bindings!");
     }
-    println!("cargo:rustc-link-search=libs/");
-    println!("cargo:rustc-link-lib=gpujpeg");
-    // let install_dir = cmake::Config::new("GPUJPEG")
-    //     .define("DCMAKE_BUILD_TYPE", "Release")
-    //     .define("DCMAKE_CUDA_ARCHITECTURES", "native")
-    //     .define("DBUILD_SHARED_LIBS", "FALSE")
-    //     .build();
-    // println!("cargo:rustc-link-search=native={}", install_dir.display());
-    // println!("cargo:rustc-link-lib=static=gpujpeg");
+    // println!("cargo:rustc-link-search=libs/");
+    // println!("cargo:rustc-link-lib=gpujpeg");
+    let install_dir = cmake::Config::new("GPUJPEG")
+        .define("DCMAKE_BUILD_TYPE", "Release")
+        .define("DCMAKE_CUDA_ARCHITECTURES", "native")
+        .define("DBUILD_SHARED_LIBS", "FALSE")
+        .build();
+    println!("install_dir:{}", install_dir.display());
+    println!(
+        "cargo:rustc-link-search=native={}/lib",
+        install_dir.display()
+    );
+    println!("cargo:rustc-link-lib=static=gpujpeg");
 }
