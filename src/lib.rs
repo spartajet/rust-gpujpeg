e438464ce99b09c9d5112e8f2f26254fef6c6771
Dr.Guo <xz.guo@hzgy-tech.com.cn>
2025-06-12 12:38:08 +0800

    Add safe wrapper types for CUDA and GPUJPEG pointers
    Implement Send and Sync for wrapper structs that encapsulate raw pointers
    
    to CUstream_st, gpujpeg_parameters, gpuj

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,23 @@
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 include!("bindings.rs");
+
+pub struct CUstreamStWarpper(pub *mut CUstream_st);
+unsafe impl Send for CUstreamStWarpper {}
+unsafe impl Sync for CUstreamStWarpper {}
+
+pub struct GpujpegParametersWarper(pub *mut gpujpeg_parameters);
+unsafe impl Send for GpujpegParametersWarper {}
+unsafe impl Sync for GpujpegParametersWarper {}
+
+pub struct GpujpegImageParametersWarrper(pub *mut gpujpeg_image_parameters);
+unsafe impl Send for GpujpegImageParametersWarrper {}
+unsafe impl Sync for GpujpegImageParametersWarrper {}
+
+pub struct GpujpegEncoderInputWarapper(pub *mut gpujpeg_encoder_input);
+unsafe impl Send for GpujpegEncoderInputWarapper {}
+unsafe impl Sync for GpujpegEncoderInputWarapper {}
+
+pub struct GpujpegEncoderWarpper(pub *mut gpujpeg_encoder);
+unsafe impl Send for GpujpegEncoderWarpper {}
+unsafe impl Sync for GpujpegEncoderWarpper {}
